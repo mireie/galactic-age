b073df1431fdff3b94bc18ee612478960dda8d25
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-06-25 10:59:58 -0700

    add earthTimeLeft test update variables to const

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -4,7 +4,7 @@ describe('GalacticAge', () => {
 
   let earthAge;
 
-test('it will return mercury age', () => {
+test('it will return Mercury age', () => {
   earthAge = new GalacticAge(33);
   expect(earthAge.getMercuryAge()).toEqual(137.50);
 });
@@ -22,6 +22,11 @@ test('it will return Mars age', () => {
 test('it will return Jupiter age', () => {
   earthAge = new GalacticAge(33);
   expect(earthAge.getJupiterAge()).toEqual(2.78);
+});
+
+test('it will return years left on Earth', () => {
+  earthAge = new GalacticAge(33);
+  expect(earthAge.earthTimeLeft()).toEqual(39.56)
 });
 
 });
--- src/js/galaCalc.js
+++ src/js/galaCalc.js
@@ -5,24 +5,25 @@ export class GalacticAge {
   }
 
   getMercuryAge() {
-    let planetYearDays = .24 * earthYearDays;
-    let planetAge = this.solarAge * earthYearDays / planetYearDays;
+    const planetYearDays = .24 * earthYearDays;
+    const planetAge = this.solarAge * earthYearDays / planetYearDays;
     return parseFloat(planetAge.toFixed(2));
   }
   getVenusAge() {
-    let planetYearDays = .62 * earthYearDays;
-    let planetAge = this.solarAge * earthYearDays / planetYearDays;
+    const planetYearDays = .62 * earthYearDays;
+    const planetAge = this.solarAge * earthYearDays / planetYearDays;
     return parseFloat(planetAge.toFixed(2));
   }
   getMarsAge() {
-    let planetYearDays = 1.88 * earthYearDays;
-    let planetAge = this.solarAge * earthYearDays / planetYearDays;
+    const planetYearDays = 1.88 * earthYearDays;
+    const planetAge = this.solarAge * earthYearDays / planetYearDays;
     return parseFloat(planetAge.toFixed(2));
   }
   getJupiterAge() {
-    let planetYearDays = 11.86 * earthYearDays;
-    let planetAge = this.solarAge * earthYearDays / planetYearDays;
+    const planetYearDays = 11.86 * earthYearDays;
+    const planetAge = this.solarAge * earthYearDays / planetYearDays;
     return parseFloat(planetAge.toFixed(2));
   }
+ 
 
 }

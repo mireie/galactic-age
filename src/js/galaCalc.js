c33bbfdfa620e1491bf5e7bbe462047c36ac47fa
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-06-25 11:01:04 -0700

    update to meet test requirements. Add Mercury test

--- src/js/galaCalc.js
+++ src/js/galaCalc.js
@@ -4,29 +4,29 @@ export class GalacticAge {
     this.solarAge = parseFloat(solarAge);
   }
 
-  getMercuryAge() {
-    const planetYearDays = .24 * earthYearDays;
-    const planetAge = this.solarAge * earthYearDays / planetYearDays;
-    return parseFloat(planetAge.toFixed(2));
-  }
-  getVenusAge() {
-    const planetYearDays = .62 * earthYearDays;
-    const planetAge = this.solarAge * earthYearDays / planetYearDays;
-    return parseFloat(planetAge.toFixed(2));
-  }
-  getMarsAge() {
-    const planetYearDays = 1.88 * earthYearDays;
-    const planetAge = this.solarAge * earthYearDays / planetYearDays;
-    return parseFloat(planetAge.toFixed(2));
-  }
-  getJupiterAge() {
-    const planetYearDays = 11.86 * earthYearDays;
-    const planetAge = this.solarAge * earthYearDays / planetYearDays;
-    return parseFloat(planetAge.toFixed(2));
-  }
-  earthTimeLeft() {
-    const lifeExpect = 72.56;
-    return lifeExpect - this.solarAge;
-  }
+  // getMercuryAge() {
+  //   const planetYearDays = .24 * earthYearDays;
+  //   const planetAge = this.solarAge * earthYearDays / planetYearDays;
+  //   return parseFloat(planetAge.toFixed(2));
+  // }
+  // getVenusAge() {
+  //   const planetYearDays = .62 * earthYearDays;
+  //   const planetAge = this.solarAge * earthYearDays / planetYearDays;
+  //   return parseFloat(planetAge.toFixed(2));
+  // }
+  // getMarsAge() {
+  //   const planetYearDays = 1.88 * earthYearDays;
+  //   const planetAge = this.solarAge * earthYearDays / planetYearDays;
+  //   return parseFloat(planetAge.toFixed(2));
+  // }
+  // getJupiterAge() {
+  //   const planetYearDays = 11.86 * earthYearDays;
+  //   const planetAge = this.solarAge * earthYearDays / planetYearDays;
+  //   return parseFloat(planetAge.toFixed(2));
+  // }
+  // earthTimeLeft() {
+  //   const lifeExpect = 72.56;
+  //   return lifeExpect - this.solarAge;
+  // }
 
 }

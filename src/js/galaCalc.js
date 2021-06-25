a5eeab26213f0614bf964e2ebf5984a880063a9a
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-06-25 13:53:29 -0700

    add planets

--- src/js/galaCalc.js
+++ src/js/galaCalc.js
@@ -6,6 +6,15 @@ export class Planet {
     this.yearLength = yearLength;
   }
 }
+const Mercury = new Planet("Mercury",87.66);
+const Venus = new Planet("Venus",226.46);
+const Earth = new Planet("Earth",365.25);
+const Mars = new Planet("Mars",686.67);
+const Jupiter = new Planet("Jupiter",4331.87);
+const Saturn = new Planet("Saturn",10,760.27);
+const Uranus = new Planet("Uranus",30,684.65);
+const Neptune = new Planet("Neptune",60,189.55);
+const Pluto = new Planet("Pluto",90,797.50);
 
 export class GalacticAge {
   constructor(solarAge) {
